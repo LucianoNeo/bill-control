30c3f1ef08580e37338ad9fece3169656519a59a
LucianoNeo <tec.info.luciano@hotmail.com>
2022-03-23 18:09:47 -0300

    functions optimizations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var gastos = {  tipo: 'lanche', valor: 15, data: '23/01'}
+var gastos = {  tipo: '', valor:'' , data: ''}
 var jan = [gastos]
 var fev = [gastos]
 var mes = [jan,fev ]
@@ -6,7 +6,7 @@ var mes = [jan,fev ]
 
 var novo = { tipo: 'parcelado', valor: 150, data: '23/01' }
 jan.push(novo)
-var novo = { tipo: 'fixa', valor: 35, data: '23/01' }
+var novo = { tipo: 'fixa', valor: 35, data: '23/02' }
 fev.push(novo)
 
 
@@ -14,28 +14,20 @@ fev.push(novo)
 function escolheMes() {
     var escolha = document.getElementById('mes')
     if (escolha.value == 'jan') {
-        var res = document.getElementById('mostraFixas');
-var elemento ='';
-for (var i = 0; i < jan.length; i++) {
-    elemento += "<tr><td>" + jan[i].data + " </td>";
-    elemento += "<td>" + jan[i].valor + "</td>";
-    elemento += "<td>" + jan[i].tipo + "</td></tr>";
+        desenhaTela(jan)
+    } else{
+        desenhaTela(fev)
   }
-res.innerHTML = elemento;
-    }else{
-        var res = document.getElementById('mostraFixas');
-var elemento ='';
-for (var i = 0; i < fev.length; i++) {
-    elemento += "<tr><td>" + fev[i].data + " </td>";
-    elemento += "<td>" + fev[i].valor + "</td>";
-    elemento += "<td>" + fev[i].tipo + "</td></tr>";
-  }
-res.innerHTML = elemento;
-    }
-    
 }
-
-
-
-
   
+
+function desenhaTela(mes) {
+    var res = document.getElementById('mostraFixas');
+var elemento ='';
+for (var i = 0; i < mes.length; i++) {
+    elemento += "<tr><td>" + mes[i].data + " </td>";
+    elemento += "<td>" + mes[i].valor + "</td>";
+    elemento += "<td>" + mes[i].tipo + "</td></tr>";
+  }
+res.innerHTML = elemento;
+}

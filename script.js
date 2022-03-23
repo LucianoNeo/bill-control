6013c76b7faf81e5232d5ee6197d3bb48dd00e71
LucianoNeo <tec.info.luciano@hotmail.com>
2022-03-23 18:25:08 -0300

    types definition

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ var fev = [gastos]
 var mes = [jan,fev ]
 
 
-var novo = { tipo: 'parcelado', valor: 150, data: '23/01' }
-jan.push(novo)
-var novo = { tipo: 'fixa', valor: 35, data: '23/02' }
-fev.push(novo)
+var novo = { tipo: 'parcelado', valor: 150, data: '10/01' }
+var novo1 = { tipo: 'fixa', valor: 45, data: '15/01' }
+var novo2 = { tipo: 'lanches', valor: 25, data: '23/01' }
+var novo3 = { tipo: 'extras', valor: 50, data: '28/01' }
+jan.push(novo,novo1,novo2,novo3)
+var novo = { tipo: 'parcelado', valor: 20, data: '05/01' }
+var novo1 = { tipo: 'fixa', valor: 35, data: '17/02' }
+var novo2 = { tipo: 'lanches', valor: 45, data: '18/02' }
+var novo3 = { tipo: 'extras', valor: 5, data: '23/02' }
+fev.push(novo,novo1,novo2,novo3)
 
 
 
